b30e676efac5ea4b1ed72196f1d45f4e4c6de415
asilbek <asilbekdavid@gmail.com>
2024-01-30 21:48:20 +0500

    fix bug

--- users/views.py
+++ users/views.py
@@ -81,11 +81,26 @@ class LogoutView(APIView):
 #         return Response(data={'message': 'Patient has been deleted!'}, status=status.HTTP_200_OK)
 #
 
-class PatientView(generics.ListCreateAPIView):
-    serializer_class = PatientsSerializer
-    queryset = Patients.objects.all()
+# class PatientView(APIView):
+#     serializer_class = PatientsSerializer
+#     queryset = Patients.objects.all()
+#     permission_classes = [permissions.AllowAny]
+#     lookup_field = 'id'
+class PatientView(APIView):
     permission_classes = [permissions.AllowAny]
-    lookup_field = 'id'
+    def get(self, request, format=None):
+        patients = Patients.objects.all()
+        serializer = PatientsSerializer(patients, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request, format=None):
+        serializer = PatientsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PatientDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PatientsSerializer
